7667a427343b34a3941094c56fa55128f43feafe
Ashoksahani <ashoksahani776@gmail.com>
2022-04-04 08:45:27 +0530

    s3-and-promise

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
+const multer = require ('multer')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(multer().any())
 
 
 mongoose.connect("mongodb+srv://taabish:lkmgsyjhwbQYgkvX@cluster0.cp3ka.mongodb.net/ash?authSource=admin&replicaSet=atlas-12xyw4-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", {
